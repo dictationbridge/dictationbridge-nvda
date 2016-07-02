da4f6b37600bfc4be14b247e70d9a1c55d8d6be3
Matt Campbell <mattcampbell@pobox.com>
2016-07-02 14:38:58 -0500

    Work so far on WSR training wizard support

---
+++ addon/appModules/speechuxwiz.py
@@ -0,0 +1,49 @@
+from appModuleHandler import AppModule
+import api
+import controlTypes
+import tones
+import ui
+import windowUtils
+from NVDAObjects.behaviors import Dialog
+from NVDAObjects.UIA import UIA
+import NVDAObjects
+import speech
+import winUser
+import time
+from logHandler import log
+
+class Wizard(Dialog):
+	role = controlTypes.ROLE_DIALOG
+
+class AppModule(AppModule):
+	def chooseNVDAObjectOverlayClasses(self, obj, clsList):
+		if obj.windowClassName == "NativeHWNDHost" and obj.role == controlTypes.ROLE_PANE:
+			clsList.insert(0, Wizard)
+
+	def event_NVDAObject_init(self, obj):
+		#When waiting for a second or two on the headset microphone radio button, an unwanted window gains focus.
+		if obj.role == controlTypes.ROLE_WINDOW and isinstance(obj, UIA) and obj.UIAElement.cachedClassName == u'CCRadioButton':
+			obj.shouldAllowUIAFocusEvent = False
+
+	def script_readTrainingText(self, gesture):
+		window = api.getForegroundObject()
+		for descendant in window.recursiveDescendants:
+			if not isinstance(descendant, UIA):
+				continue
+			try:
+				automationID = descendant.UIAElement.currentAutomationID
+			except:
+				continue
+			if automationID == "txttrain":
+				speech.speakText(descendant.name)
+				break
+
+	__gestures = {
+		"kb:`": "readTrainingText",
+	}
+
+	def event_nameChange(self, obj, nextHandler):
+		log.info("nameChange %s" % obj.name)
+		if obj.role == controlTypes.ROLE_STATICTEXT and obj.windowClassName == "DirectUIHWND":
+			speech.speakText(obj.name)
+		nextHandler()
