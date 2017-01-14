c0e8c41afd60c7dcf8cf7f7f55d19fa1eb061195
Matt Campbell <mattcampbell@pobox.com>
2017-01-13 19:21:55 -0600

    Use SendMessageTimeout when modifying the PATH environment variable. Bump the version number to 1.0-beta0.

--- addon/installTasks.py
+++ addon/installTasks.py
@@ -1,3 +1,5 @@
+import ctypes
+from ctypes import wintypes
 import os
 import sys
 import _winreg
@@ -5,7 +7,13 @@ import _winreg
 import win32con
 
 import addonHandler
+from logHandler import log
 import winUser
+
+def sendMessageTimeout(hwnd, msg, wParam, lParam, flags=0, timeout=5000):
+	dwResult = wintypes.DWORD()
+	lResult = ctypes.windll.user32.SendMessageTimeoutW(hwnd, msg, wParam, lParam, flags, timeout, ctypes.byref(dwResult))
+	return lResult, dwResult
 
 def onInstall():
 	#Add ourself to the path, so that commands when spoken can be queried to us.
@@ -21,12 +29,15 @@ def onInstall():
 	if not isinstance(dir, unicode):
 		dir = dir.decode(sys.getfilesystemencoding())
 	dir = dir.replace(addonHandler.ADDON_PENDINGINSTALL_SUFFIX, "")
+	log.info("addon directory: %r" % dir)
+	log.info("current PATH: %r" % value)
 	if value.lower().find(dir.lower()) == -1:
 		if value != "":
 			value += ";"
 		value += dir
+		log.info("new PATH: %r" % value)
 		_winreg.SetValueEx(key, "Path", None, typ, value)
-		winUser.sendMessage(win32con.HWND_BROADCAST, win32con.WM_SETTINGCHANGE, 0, u"Environment")
+		sendMessageTimeout(win32con.HWND_BROADCAST, win32con.WM_SETTINGCHANGE, 0, u"Environment")
 
 def onUninstall():
 	key = _winreg.OpenKeyEx(_winreg.HKEY_CURRENT_USER, "Environment", 0, _winreg.KEY_READ | _winreg.KEY_WRITE)
@@ -45,4 +56,4 @@ def onUninstall():
 		value = value.replace(dir + ";", "")
 		value = value.replace(dir, "")
 		_winreg.SetValueEx(key, "Path", None, typ, value)
-		winUser.sendMessage(win32con.HWND_BROADCAST, win32con.WM_SETTINGCHANGE, 0, u"Environment")
+		sendMessageTimeout(win32con.HWND_BROADCAST, win32con.WM_SETTINGCHANGE, 0, u"Environment")
