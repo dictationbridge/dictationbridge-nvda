14c2691192346ab38e3992e837d97c414880526f
derekriemer <derek.riemer@colorado.edu>
2017-02-11 10:14:49 -0700

    Add a utility to allow db to send commands to msr.

---
+++ msrUtil/msr_util.cpp
@@ -0,0 +1,21 @@
+#include <windows.h>
+#include <string>
+#include "../dictationbridge-core/client/client.h"
+
+bool isValid(char c){
+	if(' ' == c) return false;
+	if('_' == c) return true;
+	return isalpha((int)c);
+}
+
+int CALLBACK WinMain(_In_ HINSTANCE hInstance,
+	_In_ HINSTANCE hPrevInstance,
+	_In_ LPSTR lpCmdLine,
+	_In_ int nCmdShow) {
+		int i=0;
+		while(lpCmdLine[i] != '\0'){
+			if(!isValid(lpCmdLine[i++])) return 1;
+		}
+		DB_SendCommand(lpCmdLine);
+		return 0;
+}
