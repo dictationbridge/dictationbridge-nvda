b828574ff3b8fab3466ead56fa37bb4692be992a
derekriemer <derek.riemer@colorado.edu>
2017-05-24 22:37:34 -0600

    Bump version number

--- buildVars.py
+++ buildVars.py
@@ -19,7 +19,7 @@ addon_info = {
 	# Translators: Long description to be shown for this add-on on add-on information from add-ons manager
 	"addon_description" : _("""Add-on to support Windows Speech Recognition and Dragon NaturallySpeaking"""),
 	# version
-	"addon_version" : "1.0.a9",
+	"addon_version" : "1.0.a10",
 	# Author(s)
 	"addon_author" : u"The DictationBridge Team <http://dictationbridge.com/>",
 	# URL for the add-on documentation support
